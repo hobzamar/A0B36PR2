51401fe05fc28a51d5070c1a6f27db6221bd7674
Martin <Martin@147.32.219.39>
2013-03-13 14:21:52 +0100

    and now????

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -14,6 +14,6 @@ public class TicTacToe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world");
     }
 }
