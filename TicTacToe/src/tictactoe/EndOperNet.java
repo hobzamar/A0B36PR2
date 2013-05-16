53b87f092ac5328b0d379065951227720d6d86db
Martin <Martin@147.32.219.39>
2013-05-16 14:46:42 +0200

    little progress

---
+++ TicTacToe/src/tictactoe/EndOperNet.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Martin
+ */
+public class EndOperNet implements ActionListener{
+    EndWinNet E;
+    int a;
+    WindowNET l;
+
+    public EndOperNet(EndWinNet e, int a,WindowNET l){
+    this.E=e;
+    this.a=a; 
+    this.l=l;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(a!=0){    
+            WindowNET s = new WindowNET(a);
+            s.setVisible(true);  
+        }
+       l.kill();
+       E.kill();               
+       
+       
+    }
+}
